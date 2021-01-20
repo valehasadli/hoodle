ba94b37a24f14eae87af052ddcfe286a5684f77d
valehasadli <valehasadli@gmail.com>
2021-01-21 00:38:27 +0400

    Update splash_screen.dart
    bug: splash screen keyboard resize

--- lib/splash/presentation/splash_screen.dart
+++ lib/splash/presentation/splash_screen.dart
@@ -8,8 +8,12 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     CustomSize().init(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Body(),
-      appBar: AppBar(brightness: Brightness.light),
+      appBar: AppBar(
+        brightness: Brightness.light,
+        automaticallyImplyLeading: false,
+      ),
     );
   }
 }
