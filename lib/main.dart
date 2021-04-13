15274b4d473ddc1e6b31d0fdd5fade16ff4328c1
valehasadli <valehasadli@gmail.com>
2021-04-13 05:43:11 +0400

    Dependency Upgrade
    refactor: dependency upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'injections.dart' as injections;
-import 'common/presentations/blocs/observers/simple_bloc_observer.dart';
 import 'common/configs/theme.dart';
 import 'common/routes/router.gr.dart';
 import 'common/routes/guards/auth_guard.dart';
 import 'common/routes/guards/guest_guard.dart';
 
-bool useFirebaseEmulator = false;
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await injections.ensureInitialized();
-
-  Bloc.observer = SimpleBlocObserver();
 
   runApp(MyApp());
 }
